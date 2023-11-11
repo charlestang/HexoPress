87fec417e20889d7136fd7127141b32780159ec4
Charles <charlestang@foxmail.com>
2023-11-11 23:37:53 +0800

    fix: white page problem.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <script lang="ts" setup>
 import { useAppStore } from './stores/app'
-import { computed } from 'vue'
 
 const appStore = useAppStore()
-
-// global locale config
-const locale = computed(() => appStore.locale)
-
 </script>
 <template>
-  <el-config-provider :locale="locale">
-    <router-view />
-  </el-config-provider>
+  <router-view />
 </template>
