7fe862be5460218e4d64e9142f41c3de472c15c8
Charles <charlestang@foxmail.com>
2023-12-25 23:17:34 +0800

    feat: Add quick meta editing panel.

--- src/components/MetaDataDialog.vue
+++ src/components/MetaDataDialog.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
-import { computed, ref, watchEffect } from 'vue'
+import { parseFrontMatter, type FrontMatter } from '@/components/FrontMatter'
+import { ElLoading } from 'element-plus'
+import { computed, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
@@ -23,9 +25,60 @@ const showDialog = computed({
     emit('update:modelValue', value)
   }
 })
+
+const frontMatter = ref<FrontMatter>({
+  title: '',
+  permalink: '',
+  date: new Date(),
+  updated: new Date(),
+  tags: [],
+  categories: [],
+  excerpt: ''
+})
+async function onOpen() {
+  const loadingInstance = ElLoading.service({ target: 'dialog' })
+  // The blog post already exists, it is now being edited.
+  await window.site.getContent(props.sourcePath).then((content) => {
+    const parseDown = parseFrontMatter(content)
+    frontMatter.value = parseDown.data as FrontMatter
+  })
+  loadingInstance.close()
+}
+async function onSave() {
+  const loadingInstance = ElLoading.service({ target: 'dialog' })
+  loadingInstance.close()
+  showDialog.value = false
+}
 </script>
 <template>
-    <el-dialog v-model="showDialog">
-
-    </el-dialog>
+  <el-dialog v-model="showDialog" class="dialog" @open="onOpen">
+    <template #header>{{ props.sourcePath }}</template>
+    <template #footer>
+      <el-button @click="showDialog = false">{{ t('common.back') }}</el-button>
+      <el-button @click="onSave" type="primary">{{ t('common.save') }}</el-button>
+    </template>
+    <el-form label-width="140px">
+      <el-form-item :label="t('editor.title')">
+        <el-input v-model="frontMatter.title" />
+      </el-form-item>
+      <el-form-item :label="t('editor.permalink')">
+        <el-input v-model="frontMatter.permalink" />
+      </el-form-item>
+      <el-form-item :label="t('editor.date')">
+        <el-date-picker v-model="frontMatter.date" type="datetime" />
+      </el-form-item>
+      <el-form-item :label="t('editor.updated')">
+        <el-date-picker v-model="frontMatter.updated" type="datetime" />
+      </el-form-item>
+      <el-form-item :label="t('editor.categories')">
+        <el-input v-model="frontMatter.categories" />
+      </el-form-item>
+      <el-form-item :label="t('editor.tags')">
+        <el-input v-model="frontMatter.tags" />
+      </el-form-item>
+      <el-form-item :label="t('editor.excerpt')">
+        <el-input v-model="frontMatter.excerpt" type="textarea" />
+      </el-form-item>
+    </el-form>
+  </el-dialog>
 </template>
