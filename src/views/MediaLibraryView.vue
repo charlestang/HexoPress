9e4e035ed96c76cdcbdadaf8b20fc2fc1c44c400
Charles <charlestang@foxmail.com>
2024-02-17 00:14:19 +0800

    feat: Add image preview to MediaLibrary page.

--- src/views/MediaLibraryView.vue
+++ src/views/MediaLibraryView.vue
@@ -24,6 +24,13 @@ watchEffect(() => {
   <el-table :data="assets" stripe style="width: 100%; margin-bottom: 10px">
     <el-table-column prop="id" label="id"></el-table-column>
     <el-table-column prop="path" label="path"></el-table-column>
+    <el-table-column :label="t('mediaLibrary.preview')">
+      <template #default="scope">
+        <template v-if="scope.row.path.endsWith('png') || scope.row.path.endsWith('jpg')">
+          <img :src="'http://127.0.0.1:2357/' + scope.row.path" width="100px"/>
+        </template>
+      </template>
+    </el-table-column>
     <el-table-column prop="modified" label="modified"></el-table-column>
     <el-table-column prop="renderable" label="renderable"></el-table-column>
     <el-table-column prop="source" label="source"></el-table-column>
